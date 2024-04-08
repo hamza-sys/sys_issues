26ce5bd65311c3174fbc870989fe05669bf2315b
hamza <hbaba1522@gmail.com>
2024-04-08 15:20:56 +0500

    remove duplicate skeletons

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -41,7 +41,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
       setIsSubmitting(true);
       if (issue) axios.patch(`/api/issues/${issue.id}`, data);
       else await axios.post("/api/issues", data);
-      router.push("/issues");
+      router.push("/issues/list");
       router.refresh()
     } catch (err) {
       setIsSubmitting(false);
