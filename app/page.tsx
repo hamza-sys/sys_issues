e86cc2d5d5171597c7154cd3ff9f1a9d2cc47184
hamza <hbaba1522@gmail.com>
2024-04-09 14:19:48 +0500

    build the latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import Pagination from './components/Pagination'
+import LatestIssues from './LatestIssues'
 
 export default function Home({searchParams}: {searchParams: {page: string}}) {
   return (
-    <>
-    <div>Hello World</div>
-    <Pagination itemCount={100} pageSize={10} currentPage={parseInt(searchParams.page)} />
-    </>
+    <LatestIssues />
   )
 }
